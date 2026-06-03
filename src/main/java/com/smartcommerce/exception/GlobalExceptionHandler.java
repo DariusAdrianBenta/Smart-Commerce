092f460cc1bb342c58ee79c7b312e43b7caeee81
DariusAdrianBenta <dariusadrian1701@gmail.com>
2026-06-03 12:07:54 +0200

    chore: cleanup unused import, @NonNull annotations and local DB config

--- src/main/java/com/smartcommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/smartcommerce/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.smartcommerce.exception;
 
-import com.smartcommerce.exception.EmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -59,16 +58,6 @@ public class GlobalExceptionHandler {
                 .message(message)
                 .build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex) {
-        ErrorResponse error = ErrorResponse.builder()
-                .timestamp(LocalDateTime.now())
-                .status(HttpStatus.NOT_FOUND.value())
-                .message(ex.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
